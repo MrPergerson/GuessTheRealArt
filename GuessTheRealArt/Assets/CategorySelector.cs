fc6878b6f874522d7226f3913d84dc76c715d83e
Cole Pergerson <cole.pergerson@gmail.com>
2023-07-13 21:17:05 -0700

    Completed category list redesign

--- GuessTheRealArt/Assets/CategorySelector.cs
+++ GuessTheRealArt/Assets/CategorySelector.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,7 +8,8 @@ using UnityEngine.UI;
 public class CategorySelector : MonoBehaviour
 {
     public RectTransform categoryRoot;
-    public GameObject categoryPrefab;
+    //public GameObject categoryPrefab;
+    public GameObject categoryGroupPrefab;
     public Transform arrowRoot;
 
     private Button leftArrow;
@@ -20,7 +22,7 @@ public class CategorySelector : MonoBehaviour
     private void Awake()
     {
         if (categoryRoot == null) Debug.LogError("Missing CategoryRoot");
-        if (categoryPrefab == null) Debug.LogError("Missing CategoryPrefab");
+        if (categoryGroupPrefab == null) Debug.LogError("Missing CategoryPrefab");
         if (arrowRoot == null) Debug.LogError("Missing ArrowRoot");
     }
 
@@ -32,21 +34,37 @@ public class CategorySelector : MonoBehaviour
         rightArrow.onClick.AddListener(NavigateRight); 
 
         var categories = GameManager.instance.Categories;
+        int groupCount = Mathf.FloorToInt(categories.Length / 3);
+        int leftOvers = categories.Length % 3;
 
-        for (int i = 0; i < categories.Length; i++)
+        var categoryIndex = 0;
+        for (int i = 0; i <= groupCount; i++)
         {
-
-            var index = i;
             
-            var selection = Instantiate(categoryPrefab, categoryRoot.transform);
-            var categoryUI = selection.GetComponent<CategorySelection>();
-            categoryUI.button.onClick.AddListener(() => GameManager.instance.SelectCategory(index));
-            categoryUI.title.text = categories[index].name;
+            var groupGameObject = Instantiate(categoryGroupPrefab, categoryRoot.transform);
+            var selectionGroup = groupGameObject.GetComponent<CategoryGroup>();
+
+            int selectionCount = 3;
+            if (i == groupCount)
+                selectionCount = leftOvers;
+
+            var selections = selectionGroup.GetSelections(selectionCount);
+
+            
+            for (int j = 0; j < selections.Length; j++)
+            {
+                var c = categoryIndex;
+                selections[categoryIndex].button.onClick.AddListener(() => GameManager.instance.SelectCategory(c));
+                selections[categoryIndex].title.text = categories[c].name;
+                categoryIndex++;
+            }
+
+            
 
 
         }
 
-        selectionWidth = categoryPrefab.GetComponent<RectTransform>().rect.width;
+        selectionWidth = categoryGroupPrefab.GetComponent<RectTransform>().rect.width;
         minPosition = -1 * selectionWidth * (categories.Length - 1);
 
 
